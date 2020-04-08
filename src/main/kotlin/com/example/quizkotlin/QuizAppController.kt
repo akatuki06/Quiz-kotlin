e0d7ba5bd2bcd0f7d826a1b5ec181c1e3c507def
akatuki06 <akatuki0623@gmail.com>
2020-04-08 16:18:25 +0900

    fun create

--- src/main/kotlin/com/example/quizkotlin/QuizAppController.kt
+++ src/main/kotlin/com/example/quizkotlin/QuizAppController.kt
@@ -1,11 +1,23 @@
 package com.example.quizkotlin
 
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import java.util.ArrayList
+
 @RestController
 class QuizAppController {
+
+    private val quizzes = ArrayList<String>()
     @GetMapping("/show")
-    fun show():String {
-        return "Kotlin!!"
+    fun show():List<String> {
+        return quizzes
+    }
+
+    @PostMapping("/create")
+    fun create(@RequestParam question:String, @RequestParam answer:Boolean) {
+        val quiz = question + ":" + answer
+        quizzes.add(quiz)
     }
 }
